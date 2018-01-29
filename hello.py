0ed9f2e4977a06be81007b311a19eea3478714e7
Mark W. Naylor <mark.naylor.1701@gmail.com>
2018-01-28 19:34:01 -0500

    Generic message function
    Contract already exits for `hello'. Keep it, but use generic for core
    functionality. Interface public, implementation private.

--- hello.py
+++ hello.py
@@ -3,12 +3,18 @@
 # file:  hello.py
 # date:  2018-Jan-28
 
+def message(msg, name):
+    output = "{0}, {1}.".format(msg, name)
+    print(output)
+
 def hello(name="world"):
-    print("Hello, {}.".format(name))
+    message("Hello", name)
+
 
 def main():
     hello()
     hello("David")
+    message("Out the back", "Jack")
 
 if __name__ == '__main__':
     main()
